d990d21dc4af39b00c186149754bdb50e28881fe
Abinawan Bhattarai <daju.in.usa@gmail.com>
2023-01-20 20:56:03 +0545

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let wr, serverID = '820745488231301210';
+window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => { wr = req; }]);
+let stickerLib = Object.values(wr.c).find(x => x?.exports?.Z?.getStickersByGuildId), el = Object.values(wr.c).find(x => x?.exports?.Z?.getGuildEmoji), so = stickerLib.exports.Z.getStickersByGuildId(serverID), eo = el.exports.Z.getGuildEmoji(serverID), s = [], e = []
+so.forEach((x) => { s.push(`${x.name} - https://media.discordapp.net/stickers/${x.id}.png`)})
+eo.forEach((x) => {
+  if (x.animated) {
+    e.push(`${x.name} - https://media.discordapp.net/emojis/${x.id}.gif`)
+    return
+  }
+  e.push(`${x.name} - https://media.discordapp.net/emojis/${x.id}.png`)
+})
+let fs = `STICKERS:\n${s.join('\n')}\n\nEMOJIS:\n${e.join('\n')}`
+copy(fs)
